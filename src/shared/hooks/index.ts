bb00e12a8ceec1aaf11f049251ec54090230a5bb
morronee <morronees@gmail.com>
2026-04-08 23:39:46 +0300

    Реорганизует хуки в подпапку lib для лучшей структуры.

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,4 +1,4 @@
-export { useAppDispatch } from './useAppDispatch.ts';
-export { useAppSelector } from './useAppSelector.ts';
-export { useDebounce } from './useDebounce.ts';
-export { usePagination } from './usePagination.ts';
+export { useAppDispatch } from './lib/useAppDispatch.ts';
+export { useAppSelector } from './lib/useAppSelector.ts';
+export { useDebounce } from './lib/useDebounce.ts';
+export { usePagination } from './lib/usePagination.ts';
